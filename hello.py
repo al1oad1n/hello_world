6bf00c8ea342b27c91d5239d711d49c591c95549
al1oad1n <romarionsrt@gmail.com>
2021-03-14 21:47:12 +0300

    add printer

--- hello.py
+++ hello.py
@@ -2,9 +2,17 @@ import datetime as dt
 
 
 def greet():
-    print(f"hello Roma, current time {dt.datetime.now()}")
-    
+    print(f"good luck have fun ROMA, current time {dt.datetime.now()}")
 
 
-if __name__ == "__main__":
-    greet()
+
+# if __name__ == "__main__":
+#     greet()
+def printer(n):
+    for i in range(1,n+1):
+        print(f"current number {i}") 
+
+print("enter number:")
+a = int(input())
+
+printer(a)
